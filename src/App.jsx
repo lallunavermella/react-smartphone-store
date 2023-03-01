ce95e52bb8fefc103d7849322fce05660d0a72e0
Silvia Martinez <s.marti.carbonell@gmail.com>
2023-03-01 20:45:59 +0100

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import ProductDetailsPage from './pages/ProductDetailsPage';
+import ProductListPage from './pages/ProductListPage';
 
 function App() {
   return (
     <div className="flex justify-center items-center h-full w-full text-sm">
-      HELLO WORLD
+      <Routes>
+        <Route path="/" element={<ProductListPage />} />
+        <Route path="/product/:product" element={<ProductDetailsPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
