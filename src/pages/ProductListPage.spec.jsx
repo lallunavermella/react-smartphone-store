416a01f115c53253528dbf6d72ea71899757006d
Silvia Martinez <s.marti.carbonell@gmail.com>
2023-03-03 00:32:43 +0100

    Add Search and ProductListItem in ProductListPage and update tests

--- src/pages/ProductListPage.spec.jsx
+++ src/pages/ProductListPage.spec.jsx
@@ -1,19 +1,23 @@
 import { screen, waitFor } from '@testing-library/react';
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import renderWithProviders from '../utils/utils-test';
 import ProductListPage from './ProductListPage';
 
 describe('Given a ProductList page', () => {
   describe('When is invoked', () => {
-    test.only('Then its render with ....', async () => {
-      renderWithProviders(<ProductListPage />);
+    test('Then its render with ....', async () => {
+      renderWithProviders(<MemoryRouter><ProductListPage /></MemoryRouter>);
 
       let text;
+      let link;
       await waitFor(() => {
-        text = screen.getByText('2');
+        text = screen.getByText('Product list: 2');
+        link = screen.getAllByRole('link');
       });
 
       expect(text).toBeInTheDocument();
+      expect(link.length).toBe(2);
     });
   });
 });
