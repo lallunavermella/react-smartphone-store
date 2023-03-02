45212e19bb63dda225989fa98e4f96d77cece508
Silvia Martinez <s.marti.carbonell@gmail.com>
2023-03-03 00:31:52 +0100

    Create component Search and ProductListItem

---
+++ src/components/ProductListItem/ProductListItem.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function ProductListItem({
+  product: {
+    id, imgUrl, brand, model, price,
+  },
+}) {
+  return (
+    <Link to={`/product/${id}`}>
+      <div className="w-full h-[300px] bg-blue-200 flex flex-col justify-center items-center p-2">
+        <img loading="lazy" src={imgUrl} alt={id} />
+        <p>{brand}</p>
+        <p>{model}</p>
+        <p>{price}</p>
+
+      </div>
+    </Link>
+  );
+}
+
+ProductListItem.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    imgUrl: PropTypes.string.isRequired,
+    brand: PropTypes.string.isRequired,
+    model: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default ProductListItem;
